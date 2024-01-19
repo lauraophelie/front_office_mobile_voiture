1254c5fa8e20db85facf058954ddffb2871aba7e
Laura Ophélie <lauraophelie1@gmail.com>
2024-01-19 14:36:32 +0100

    login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,46 @@
+import { IonPage } from "@ionic/react";
+
+import "./Login.scss";
+import Input from "../../components/input/Input";
+import Bouton from "../../components/bouton/Bouton";
+
 const Login: React.FC = () => {
     return (
-        <>
-        </>
+        <IonPage>
+            <div className="form">
+                <h1 className="form__title">
+                    Login
+                    <p className="form__p">
+                        Connectez-vous à votre compte
+                    </p>
+                </h1>
+
+                <div className="form__input">
+                    <Input 
+                        type={"email"} 
+                        placeholder={"Adresse e-mail"}
+                        className="form__input__text"
+                        required={true}
+                    />
+
+                    <Input 
+                        type={"password"} 
+                        placeholder={"Mot de passe"}
+                        className="form__input__text"
+                        required={true}
+                    />
+
+                    <Bouton 
+                        text={"Se connecter"} 
+                        className="form__input__button"
+                    />
+                </div>
+
+                <p className="form__p form__p--footer">
+                    Pas encore de compte ? Inscrivez-vous
+                </p>
+            </div>
+        </IonPage>
     )
 }
 
