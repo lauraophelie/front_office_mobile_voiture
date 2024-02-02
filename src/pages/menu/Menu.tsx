1def18a089513456d99b255aae2feab647af9197
Laura Ophélie <lauraophelie1@gmail.com>
2024-02-02 15:15:43 +0100

    modifications

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -1,10 +1,42 @@
-import { IonContent } from "@ionic/react";
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent } from "@ionic/react";
 import "./Menu.scss";
+import "./card/card.scss";
 
 const Menu: React.FC = () => {
     return (
         <IonContent>
-            
+            <div className="menu">
+                <div className="menu__header">
+                    <h1 className="menu__title">
+                        <p className="menu__title--p">
+                            Bienvenu(e),
+                        </p>
+                        <p className="menu__title--h">
+                            Nom utilisateur
+                        </p>
+                    </h1>
+                </div>
+
+                <div className="menu__content">
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonCardTitle>Card Title</IonCardTitle>
+                            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                        </IonCardHeader>
+
+                        <IonCardContent>Card Content</IonCardContent>
+                    </IonCard>
+
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonCardTitle>Card Title</IonCardTitle>
+                            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                        </IonCardHeader>
+
+                        <IonCardContent>Card Content</IonCardContent>
+                    </IonCard>
+                </div>
+            </div>
         </IonContent>
     )
 }
