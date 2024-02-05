810eeebc2c736376f87a6d584fc9ec222bb91366
Laura Ophélie <lauraophelie1@gmail.com>
2024-02-05 13:45:59 +0100

    annonces

--- src/pages/annonce/liste/DetailsAnnonce.tsx
+++ src/pages/annonce/liste/DetailsAnnonce.tsx
@@ -1,7 +1,24 @@
+import { IonContent } from "@ionic/react";
+import { useLocation } from "react-router";
+
 const DetailsAnnonce: React.FC = () => {
+    const location = useLocation<{ id: number }>();
+    const annonceId = location.state.id;
+
+    console.log("Annonce : " + annonceId);
+
     return (
-        <>
-        </>
+        <IonContent>
+            <div className="details-annonce">
+                <div className="details-annonce__image">
+                    images
+                </div>
+
+                <div className="details-annonce__content">
+                    
+                </div>
+            </div>
+        </IonContent>
     )
 }
 
